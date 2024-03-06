8fced1a018d644683316b8b47a9711234d1ea973
coutomanu <141460517+coutomanu@users.noreply.github.com>
2024-03-05 21:54:13 -0300

    Add files via upload

---
+++ BIBLIOTECH/src/Livro.java
@@ -0,0 +1,16 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String editora;
+    private Integer anoPub;
+    private Integer isbn;
+
+    public Livro(String titulo, String autor, String editora, Integer anoPub, Integer isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.anoPub = anoPub;
+        this.isbn = isbn;
+    }
+}
+
---
+++ BIBLIOTECH/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String email;
+
+    public Usuario(String nome, String endereco, String telefone, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.email = email;
+    }
+}
